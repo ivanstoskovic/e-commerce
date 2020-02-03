eb14988112bbb8c2c4c14148ffe1474c22318d8d
Ivan Stoskovic <ivst0406@enjoying.rs>
2020-02-03 17:20:31 +0100

    comment for selector

--- src/REDUX/cart/cart-selectors.js
+++ src/REDUX/cart/cart-selectors.js
@@ -2,8 +2,8 @@ import { createSelector } from 'reselect';
 
 //ovo je input selector i ne koristi createSelector i uzima sve iz state-a sto zelimo (state.cart)
 const selectCart = state => state.cart;
-
-//Ovo je output selector i sluzi zapravo da uradi Memoization(ne memorirzation) - ne renderuje komponentu ako vrednost objekta koji se prosledjuje komponenti nije izmenjena
+//createSelector funkcija zapravo radi Memoization
+//Ovo je output selector i sluzi zapravo da uradi Memoization(ne memorirzation) {ne renderuje komponentu ako vrednost objekta koji se prosledjuje komponenti nije izmenjena}
 export const selectCartItems = createSelector(
     [selectCart],
     cart => cart.cartItems
